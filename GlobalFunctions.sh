494dc68e08aa0b4c688a2b032d19d62d77d6a87e
gorodok11 <gorodok11@gmail.com>
2013-03-22 22:34:39 +0400

    Update GlobalFunctions.sh

--- GlobalFunctions.sh
+++ GlobalFunctions.sh
@@ -11,3 +11,17 @@ install_packages()
 		exit 1
 	fi
 }
+
+#Функция для проаерки подключения к интернету
+ping_google()
+{
+echo "Проверка подключения к интернету (ping google.com)"
+ping google.com -c1 2>&1 >> /dev/null
+if [ $? -eq 0 ]; then
+	echo "Есть подключение!"
+else
+	echo "При проверке связи не удалось обнаружить узел www.google.com."
+	
+	exit 1
+fi
+}
