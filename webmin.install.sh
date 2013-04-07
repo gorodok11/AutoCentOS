96363011b1606e3b20c457797db815cf79cc6933
gorodok11 <gorodok11@gmail.com>
2013-04-07 20:58:52 +0400

    Update webmin.install.sh

--- webmin.install.sh
+++ webmin.install.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-#WebMin - приложение для облегчения управления сервером через WEB консоль
-#Когда Webmin установлен из репозитория, установка исправлений и патчей может быть автоматизирована как и другие системные обновления, 
-#и управляться при помощи YUM. Кроме того, демон yum-updates предупредит вас об обновлениях и(или) установит их в зависимости от заданных параметров.
-echo "Установка WebMin..."
+########################################################
+#     ___       __       _____         __  ____  ____  #
+#    / _ |__ __/ /____  / ___/__ ___  / /_/ __ \/ __/  #
+#   / __ / // / __/ _ \/ /__/ -_) _ \/ __/ /_/ /\ \    #
+#  /_/ |_\_,_/\__/\___/\___/\__/_//_/\__/\____/___/    #
+#                                                      #
+#                     Frunza Igor                      #
+#                         2013                         #
+########################################################
+
+# Some copyright info:
+# Logo created with http://patorjk.com/software/taag/
+tput setaf 2
+echo "Install WebMin..."
+tput sgr0
 echo "[Webmin]
 name=Webmin Distribution Neutral
 baseurl=http://download.webmin.com/download/yum
 enabled=1" >/etc/yum.repos.d/webmin.repo
 rpm --import http://www.webmin.com/jcameron-key.asc
 yum -y install webmin
-echo "=================================================="
+
+
