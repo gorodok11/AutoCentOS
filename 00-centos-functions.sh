433e62b9af257ca6fc3fcc9a104f905a150f6872
gorodok11 <gorodok11@gmail.com>
2013-04-07 22:17:06 +0400

    Update 00-centos-functions.sh

--- 00-centos-functions.sh
+++ 00-centos-functions.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+########################################################
+#     ___       __       _____         __  ____  ____  #
+#    / _ |__ __/ /____  / ___/__ ___  / /_/ __ \/ __/  #
+#   / __ / // / __/ _ \/ /__/ -_) _ \/ __/ /_/ /\ \    #
+#  /_/ |_\_,_/\__/\___/\___/\__/_//_/\__/\____/___/    #
+#                                                      #
+#                     Frunza Igor                      #
+#                         2013                         #
+########################################################
+
+# Some copyright info:
+# Logo created with http://patorjk.com/software/taag/
+
 # This file contains global functions
 # To use these functions just import this file 
 # source 00-centos-functions.sh
