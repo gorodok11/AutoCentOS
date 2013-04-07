b509cd55b29ba6f63e7696c2a016cc6269cccb79
gorodok11 <gorodok11@gmail.com>
2013-04-07 13:53:03 +0400

    Update 00-centos-functions.sh

--- 00-centos-functions.sh
+++ 00-centos-functions.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 # This file contains global functions
 # To use these functions just import this file 
-# source oo-centos-functions.sh
+# source 00-centos-functions.sh
+
+#Declaring Functions - This function is an easier way to reuse the yum code. 
+install_packages()
+{
+ tput setaf 2
+ echo "Installing packages: ${@}"
+ tput sgr0
+ yum -y update && yum -y install ${@}
+ if [ $? -eq 0 ]; then
+  tput setaf 2
+  echo "Packages successfully installed."
+  tput sgr0
+ else
+  tput setaf 1
+  echo "Packages failed to install!"
+  tput sgr0
+  exit 1
+ fi
+}
+
+#----------------------------------------------------
+
